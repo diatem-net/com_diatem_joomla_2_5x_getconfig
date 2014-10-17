4f1681ff990f1b86d2cc2ff4af037a546689546a
Loïc Gerard <lgerard@diatem.net>
2014-10-17 08:36:37 +0200

    Correctifs view configuration

--- administrator/views/apropos/tmpl/default.php
+++ administrator/views/apropos/tmpl/default.php
@@ -1,5 +1,7 @@
 <h1>Diatem - Joomla! getConfig</h1>
 <b>Pour Joomla! 2.5.x</b><br>
-<p>Composant pour Joomla!. Implémente un service REST sécurisé permettant de récupérer à distance la version de Joomla! ainsi que des modules, composants et plugins utilisés. Services accessibles avec l'Url [UrlSite]components/com_diatem_joomla_3x_getconfig/ en GET.</p>
+<p>Composant pour Joomla!. Implémente un service REST sécurisé permettant de récupérer à distance la version de Joomla! ainsi que des modules, composants et plugins utilisés. Services accessibles avec l'Url [UrlSite]components/com_diatem_joomla_2_5x_getconfig/ en GET.</p>
 <p>L'accès se fait via une authentification par clé publique / clé privée. La configuration du couple clé publique/clé privée du service est à effectuer dans Extensions/gestion des extensions/Paramètres/Diatem Joomla(3.x.x) getConfig/<br>
-<a href="http://www.ciarus.com/administrator/index.php?option=com_config&view=component&component=com_diatem_joomla_2_5x_getconfig&path=&tmpl=component">Accéder à la configuration de la clé publique/clé privée</a>
+<a class="modal" rel="{handler: 'iframe', size: {x: 875, y: 550}, onClose: function() {}}" href="http://www.alsace-qualite.com/administrator/index.php?option=com_config&view=component&component=com_diatem_joomla_2_5x_getconfig&path=&tmpl=component">
+Accéder à la configuration de la clé publique/clé privée
+</a>
